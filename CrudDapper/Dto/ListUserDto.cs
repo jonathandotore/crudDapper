d215e0bbb370c9e2e51330df214b21745c64d578
Jonathan Dotore <jonathandotore@outlook.com>
2025-03-21 17:32:28 -0300

    Creating map for the objects that represents my user class and my Dto user, that will be returned on response model

---
+++ CrudDapper/Dto/ListUserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CrudDapper.Dto
+{
+    public class ListUserDto
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string JobTitle { get; set; }
+        public double Salary { get; set; }
+        public bool Status { get; set; }
+    }
+}

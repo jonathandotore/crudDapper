05e746b4d41bc6c5e5a6814ad3fbddf0662ce524
Jonathan Dotore <jonathandotore@outlook.com>
2025-03-24 17:49:39 -0300

    Adding environment URL's and creating initial configs for Angular

---
+++ AngularDapper/src/app/models/user.ts
@@ -0,0 +1,10 @@
+export interface UserList {
+    id?: null,
+    fullName: string,
+    email: string,
+    jobTitle: string,
+    salary: number,
+    cpf: string,
+    status: boolean,
+    password: string
+}
---
+++ AngularDapper/src/app/services/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserList } from '../models/user';
+import { Response } from '../models/response'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  ApiUrl = environment.UrlApi;
+
+  constructor(private http : HttpClient) { }
+
+  GetUsers() : Observable<Response<UserList[]>> {
+    return this.http.get<Response<UserList[]>> (`${this.ApiUrl}User`)
+  }
+}

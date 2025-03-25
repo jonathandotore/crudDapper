6de515f64762d6b198f0aaf68323efe2a60a0084
Jonathan Dotore <jonathandotore@outlook.com>
2025-03-25 17:06:16 -0300

    [NEW] Adding forms properties and handling data forms

--- AngularDapper/src/app/components/forms/forms.component.ts
+++ AngularDapper/src/app/components/forms/forms.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 @Component({
   selector: 'app-forms',
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule, ReactiveFormsModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.css'
 })
 
-export class FormsComponent {
+export class FormsComponent implements OnInit {
 
+  userForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.userForm = new FormGroup({
+      id: new FormControl(0),
+      fullName: new FormControl(''),
+      email: new FormControl(''),
+      jobTitle: new FormControl(''),
+      salary: new FormControl(0),
+      cpf: new FormControl(''),
+      status: new FormControl(true),
+      password: new FormControl('')
+    });
+  }
+
+  submit() {
+    console.log(this.userForm.value);
+  }
 }

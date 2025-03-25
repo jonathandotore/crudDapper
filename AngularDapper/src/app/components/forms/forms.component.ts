960aac68a41690d24a2ba27772d2ab71908b4941
Jonathan Dotore <jonathandotore@outlook.com>
2025-03-25 15:50:40 -0300

    [NEW] Creating and editing forms component and passing trough register page

---
+++ AngularDapper/src/app/components/forms/forms.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-forms',
+  imports: [RouterModule],
+  templateUrl: './forms.component.html',
+  styleUrl: './forms.component.css'
+})
+
+export class FormsComponent {
+
+}
---
+++ AngularDapper/src/app/pages/register/register.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormsComponent } from '../../components/forms/forms.component';
+
+@Component({
+  selector: 'app-register',
+  imports: [FormsComponent],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+
+}

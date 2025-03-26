d788a64164766edd3584465d5f60869011a2a60b
Jonathan Dotore <jonathandotore@outlook.com>
2025-03-26 16:56:51 -0300

    Crud with dapper finished

--- AngularDapper/src/app/app.routes.ts
+++ AngularDapper/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { EditComponent } from './pages/edit/edit.component';
+import { DetailsComponent } from './pages/details/details.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'register', component: RegisterComponent },
-    { path: 'edit/:id', component: EditComponent }
+    { path: 'edit/:id', component: EditComponent },
+    { path: 'details/:id', component: DetailsComponent }
 ];

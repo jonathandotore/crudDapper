8c5463a97d383690f8d07bbac5239fbcf75fb75c
Jonathan Dotore <jonathandotore@outlook.com>
2025-03-26 01:04:19 -0300

    Adding initial edit user implementation

---
+++ AngularDapper/src/app/pages/edit/edit.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormsComponent } from "../../components/forms/forms.component";
+import { UserService } from '../../services/user.service';
+import { UserList } from '../../models/user';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-edit',
+  imports: [FormsComponent, CommonModule],
+  templateUrl: './edit.component.html',
+  styleUrl: './edit.component.css'
+})
+
+export class EditComponent {
+
+  user!: UserList;
+
+
+  constructor(private userService: UserService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    
+    const id = this.route.snapshot.paramMap.get('id') as string;
+
+    this.userService.GetUserById(id).subscribe(response => {
+      this.user = response.data;
+    });
+  }
+}
